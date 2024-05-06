f951dbf0e2e2e1a367ca997c6a17582c6b234752
JustFast <realbobrossthepainter@gmail.com>
2024-05-06 18:53:04 +0000

    buggin

--- src/testproc.c
+++ src/testproc.c
@@ -1,9 +1,11 @@
 #include "../include/rtfnk/proc.h"
 #include "../include/rtfnk/pcb.h"
+#include "../lib/util/flinkedlist.h"
 
 unsigned int proc;
 struct fnk_pcb* pcbs;
 struct fnk_pcb* self;
+struct util_flinkedlist list[50];
 
 /**
  * It is assumed that ALL programs will use a calling convention for the main function that treats its arguments as strictly on the stack
@@ -14,6 +16,8 @@ int main(unsigned int argproc, struct fnk_pcb argpcbs[]) {
     pcbs = argpcbs;
 
     self = &(pcbs[proc]);
+
+    util_flinkedlist_init(list, sizeof(list) / sizeof(list[0]));
 
     while (1) {
         // Do something!
